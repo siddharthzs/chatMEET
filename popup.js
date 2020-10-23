b05d5c30f7ee1d7bbda1d9627ab30832ca8c413d
Siddharth Choudhary <60484636+siddharthzs@users.noreply.github.com>
2020-10-24 00:12:16 +0530

    Used prettier to make code readable
    Maintainer, you really need to look at your code readability

--- popup.js
+++ popup.js
@@ -1,41 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.sync.get(["enable_notifications"], function (item) {
+    document.getElementById("n_i").checked = item["enable_notifications"];
+  });
 
-
-
-
-document.addEventListener("DOMContentLoaded", function(){
-
-
-    chrome.storage.sync.get(['enable_notifications'], function(item){
-
-
-    
- document.getElementById("n_i").checked =  item['enable_notifications'];
-
+  document.getElementById("n_i").addEventListener("click", function () {
+    chrome.storage.sync.get(["enable_notifications"], function (item) {
+      if (item["enable_notifications"] == true) {
+        console.log("i am");
+        document.getElementById("n_i").checked = false;
+        chrome.storage.sync.set({ enable_notifications: false }, () => {});
+      } else {
+        document.getElementById("n_i").checked = true;
+        chrome.storage.sync.set({ enable_notifications: true }, () => {});
+      }
     });
-
-
-
-    document.getElementById("n_i").addEventListener("click", function(){
-
-    chrome.storage.sync.get(['enable_notifications'], function(item){
-        if(item['enable_notifications'] == true){
-            console.log("i am");
-            document.getElementById("n_i").checked =  false;
-        chrome.storage.sync.set({ 'enable_notifications':  false }, ()=>{});
-        
-        }
-        else{
-        
-        document.getElementById("n_i").checked =  true;
-        chrome.storage.sync.set({ 'enable_notifications':  true }, ()=>{});
-        
-        }
-    });
-
-
-        
-   
+  });
 });
-
-    });
-
